246fec799563385a81600878ba26b520573da290
Roxana Solano <roxansolan@gmail.com>
2025-05-07 11:06:56 +0200

    add main

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const play = document.querySelector(".js_selecplay");
+const singleButton = document.querySelector(".js_button");
+const text = document.querySelector(".text");
+const playerPoint = document.querySelector(".playerPoint");
+const computerPoint = document.querySelector(".computerPoint");
+let player = "";
+let computer = "";
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
