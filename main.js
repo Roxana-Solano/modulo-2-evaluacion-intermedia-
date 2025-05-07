902ad937981b1bd475e88db5c534eb0177018a76
Roxana Solano <roxansolan@gmail.com>
2025-05-07 11:46:50 +0200

    add styles

--- main.js
+++ main.js
@@ -11,3 +11,48 @@ let computer = "";
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+function searchWinner() {
+  if (player === "paper" && computer === "stone") {
+    text.innerHTML = " ¡Has Ganado!";
+    playerPoint.innerHTML = parseInt(playerPoint.innerHTML) + 1;
+  } else if (player === "paper" && computer === "scissors") {
+    text.innerHTML = " ¡Has perdido! ";
+    computerPoint.innerHTML = parseInt(computerPoint.innerHTML) + 1;
+  } else if (player === "scissors" && computer === "stone") {
+    text.innerHTML = " ¡Has perdido! ";
+    computerPoint.innerHTML = parseInt(computerPoint.innerHTML) + 1;
+  } else if (player === "scissors" && computer === "paper") {
+    text.innerHTML = " ¡Has ganado! ";
+    playerPoint.innerHTML = parseInt(playerPoint.innerHTML) + 1;
+  } else if (player === "stone" && computer === "paper") {
+    text.innerHTML = " ¡Has perdido! ";
+    computerPoint.innerHTML = parseInt(computerPoint.innerHTML) + 1;
+  } else if (player === "stone" && computer === "scissors") {
+    text.innerHTML = " ¡Has ganado! ";
+    playerPoint.innerHTML = parseInt(playerPoint.innerHTML) + 1;
+  } else {
+    text.innerHTML = "Empate";
+  }
+}
+
+function getResultComputer(result) {
+  if (result <= 3) {
+    computer = "stone";
+  } else if (result >= 7) {
+    computer = "paper";
+  } else {
+    computer = "scissors";
+  }
+}
+
+singleButton.addEventListener("click", () => {
+  const result = getRandomNumber(9);
+  getResultComputer(result);
+  console.log(player, computer);
+  searchWinner();
+});
+
+play.addEventListener("change", (ev) => {
+  ev.preventDefault();
+  player = play.value;
+});
